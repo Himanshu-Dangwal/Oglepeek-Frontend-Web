1eaf24d81ac068ec97e98790b840b503b2373e77
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-06-15 17:54:25 -0700

    Added the page

---
+++ src/Components/Footer/FooterDetails.js
@@ -0,0 +1,29 @@
+export const services = [
+  {
+    labels: "Store Locator",
+  },
+  {
+    labels: "Buying Guide",
+  },
+  {
+    labels: "Frame Size",
+  },
+];
+
+export const about = [
+  {
+    labels: "Refer & Earn",
+  },
+  {
+    labels: "About Us",
+  },
+  {
+    labels: "Oglepeek Coupons",
+  },
+];
+
+export const helps = [
+  {
+    labels: "FAQ's",
+  },
+];
